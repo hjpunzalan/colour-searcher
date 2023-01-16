4a3ff8e9adaa87aa92a8b9b37ca4f8fa2134090c
hjpunzalan <hj.punzalan@hotmail.com>
2023-01-16 20:08:52 +0800

    Search bar test colour display and refactor other test

--- src/__test__/SearchBar.test.tsx
+++ src/__test__/SearchBar.test.tsx
@@ -5,12 +5,30 @@ import { render, screen } from '@testing-library/react';
 test('SearchBar should display label, input text and colour', async () => {
   render(<SearchBar colour="" setColour={jest.fn()} disabled={false} />);
 
-  let inputText = await screen.findByLabelText(/colour/i);
-  expect(inputText).toBeInTheDocument();
-
-  inputText = await screen.findByTestId('colour-search');
-  expect(inputText).toBeInTheDocument();
+  const inputText = await screen.findByLabelText(/colour/i);
+  expect(inputText).toHaveAttribute('type', 'text');
 
   const inputColor = await screen.findByTestId('colour-picker');
-  expect(inputColor).toBeInTheDocument();
+  expect(inputColor).toHaveAttribute('type', 'color');
+});
+
+test('Disable search bar and colour picker if disabled is true', async () => {
+  render(<SearchBar colour="" setColour={jest.fn()} disabled={true} />);
+
+  const inputText = await screen.findByTestId('colour-search');
+  expect(inputText).toBeDisabled();
+
+  const inputColor = await screen.findByTestId('colour-picker');
+  expect(inputColor).toBeDisabled();
+});
+
+test('Colour displayed in input text and input color', async () => {
+  const colour = '#ff0000';
+  render(<SearchBar colour={colour} setColour={jest.fn()} disabled={false} />);
+
+  const inputText = await screen.findByLabelText(/colour/i);
+  expect(inputText).toHaveAttribute('value', colour);
+
+  const inputColor = await screen.findByTestId('colour-picker');
+  expect(inputColor).toHaveAttribute('value', colour);
 });
