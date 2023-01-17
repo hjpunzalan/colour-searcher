6cc0b38b49582c2f73236c39188c528326d67139
hjpunzalan <hj.punzalan@hotmail.com>
2023-01-17 11:18:25 +0800

    move common components

--- src/components/ColourResults.tsx
+++ src/components/ColourResults.tsx
@@ -27,7 +27,8 @@ export const ColourResults: React.FC<Props> = ({ searchColour, rawColours, colou
             // https://swr.vercel.app/docs/mutation#revalidation
             // mutate will revalidate fetch
             mutate(XKCD_JSON);
-          }}>
+          }}
+        >
           Refetch
         </Button>
       </Stack>
